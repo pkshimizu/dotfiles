a346336553538e4badb273b49d43d63fae46335c
vagrant <vagrant@precise64.(none)>
2014-07-20 15:25:25 +0000

    cp -> ln

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,8 +1,8 @@
-cp dotfiles/.gemrc ./
+ln -s ~/dotfiles/.gemrc ~/.gemrc
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_history
+source ~/.bash_profile
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 2.1.1 
 rbenv global 2.1.1
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,4 @@
-cp dotfiles/.vimrc ./
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 sudo apt-get install vim
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
